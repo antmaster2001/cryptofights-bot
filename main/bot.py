ddf44c37dda49f2cba743caec618e559f9eeee89
antmaster2001 <gijsbakker@hotmail.nl>
2021-11-06 23:20:02 +0100

    Start step plan

--- main/bot.py
+++ main/bot.py
@@ -8,7 +8,7 @@ import random
 import pydirectinput
 
 
-class Fighter:
+class Game:
     def __init__(self):
         self.stc = mss.mss()
 
@@ -16,14 +16,8 @@ class Fighter:
         self.img_path = os.path.join(path, 'img')
         self.mouse = mouse.Controller()
         self.keyboard = keyboard.Controller()
+
         self.bar_top = 0
         self.bar_left = 0
 
-        # Increase this limit if you have a larger basket
-        self.fish_count = 0
-        self.fish_limit = 6
-        self.keep_fishing = True
-		
-        # Adding spot to update sell thresholds!
-        self.sell_threshold = .8
-
+    
---
+++ main/main.py
@@ -0,0 +1,13 @@
+#Step pla for the bot
+# 1. Loop starts with entering the dungeon from the lobby
+# 2. When start program set a level you want to farm, {world: 0, level: 4} (this would be "enchanted forest", level 4 Ogre)
+# 3. Click the enter button to start the level
+# 4. Detect if the match is made (If the game gets cancelled you get set back to step 2), else the match is starting 
+#    
+# 
+# 
+# 
+# 
+# 
+# 
+#
